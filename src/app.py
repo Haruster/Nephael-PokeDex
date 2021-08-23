df4c4dbf46a8a7726ed667bd80fd6271a84a5fff
Nephael <67867993+Nephael@users.noreply.github.com>
2021-08-23 22:57:10 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -9,10 +9,10 @@ from io import BytesIO
 
 window = tk.Tk();
 window.geometry("600x500")
-window.title("Kinesys PokeDex")
+window.title("Nephael PokeDex")
 window.config(padx = 10, pady = 10)
 
-title_label = tk.Label(window, text = "Kinesys PokeDex")
+title_label = tk.Label(window, text = "Nephael PokeDex")
 title_label.config(font = ("Arial", 32))
 title_label.pack(padx = 10, pady = 10)
 
